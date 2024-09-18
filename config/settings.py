783a8def8df0dd05ee2b5c1a43e52d1cf78ff4e4
Rui-Yamaguchi <rui.yamaguchi1122@gmail.com>
2024-09-18 09:56:23 +0900

    setteingにセット

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5v8fk6vd*!ab*5tpxkk##=bo7y6hw$nr#-agk2eniixv+&6-@v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'todo-list-yd1f.onrender.com',
+]
 
 
 # Application definition
